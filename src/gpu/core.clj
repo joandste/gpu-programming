3540c59c121581f8a121d288b4dae5432f03c15c
Jonathan <joandste@abo.fi>
2024-11-02 03:32:17 +0200

    something....

--- src/gpu/core.clj
+++ src/gpu/core.clj
@@ -1,23 +1,34 @@
 (ns gpu.core
   (:require [uncomplicate.clojurecl.core :refer :all] 
             [uncomplicate.clojurecl.info :refer :all]
-            [clojure.math :refer [acos sin cos]]
+            [clojure.math :refer [acos sin cos PI]]
             [clojure.string :refer [split]]
             [clojure.java.io :refer [reader]]))
 
 
-(defn parse-file [file re]
+(defn parse-file-data [file re]
   (map (fn [line] (map Double/parseDouble (split line re)))
      (next (line-seq 
       (reader file)))))
 
+(defn parse-file-count [file] 
+       (first (line-seq
+              (reader file))))
+
 (defn arc-to-rad [data]
-  (map (fn [coordinates] (map (fn [arc] (* arc 0.000291)) coordinates)) data))
+  (let [conversion (/ (* (/ 1 60) PI) 180)]
+   (map (fn [coordinates] (map (fn [arc] (* arc conversion)) coordinates)) data)))
 
 (defn calculate [coord1 coord2]
   (let [[asc1 dec1] coord1
         [asc2 dec2] coord2]
-   (acos (+ (* (sin dec1) (sin dec2)) (* (cos dec1) (cos dec2) (cos (- asc1 asc2)))))))
+   (acos (+ (* (sin dec1) (sin dec2))
+            (* (cos dec1) (cos dec2) (cos (- asc1 asc2)))))))
+
+(defn calculate-pairs [list1 list2]
+  (mapcat (fn [x]
+            (map (fn [y] (calculate x y)) list2))
+          list1))
 
 
 (defn -main []
@@ -25,17 +36,20 @@
 
 
 (comment
-  (def data (parse-file "data_100k_arcmin.dat" #"\t"))
+  (def data (arc-to-rad (parse-file-data "data_100k_arcmin.dat" #"\t")))
   
-  (def rand (parse-file "rand_100k_arcmin.dat" #" "))
+  (def rand (arc-to-rad (parse-file-data "rand_100k_arcmin.dat" #" ")))
   
-  (first (arc-to-rad data))
-  (first (arc-to-rad rand))
+  (take 10 data)
+  (first data)
+
+  (parse-file-count "data_100k_arcmin.dat")
+  (parse-file-count "rand_100k_arcmin.dat")
   
-  (def dd (pmap calculate (arc-to-rad data) (arc-to-rad data)))
-  (def dr (pmap calculate (arc-to-rad data) (arc-to-rad rand)))
-  (def rr (pmap calculate (arc-to-rad rand) (arc-to-rad rand)))
+  (def dd (calculate-pairs data data))
+  (def dr (calculate-pairs data rand))
+  (def rr (calculate-pairs rand rand))
   
   (take 100 dd)
-
+  (take 100 rr) 
   (first dd))
